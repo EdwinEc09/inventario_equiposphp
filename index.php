fa9e99ddcd66e48e22689e2f4f8258884c4d8711
Edwin <edwin@gmail.com>
2024-09-14 11:55:36 -0500

    se hacen cambios en la paginacion y el luyoout

--- index.php
+++ index.php
@@ -3,52 +3,71 @@
 // require 'settings.php';
 
 // Determinar la página solicitada
-$page = isset($_GET['page']) ? $_GET['page'] : 'login';
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
 
-// Incluir el archivo de layout
-// require 'layout.php';
+// Páginas que NO extienden el layout
+// $noLayoutPages = ['login'];
+$noLayoutPages = ['login', 'registro','email-verification','password-reset-2','password-reset','static-non-auth'];
 
-// Incluir el contenido específico basado en la página solicitada
-switch ($page) {
-    case 'home':
-        require 'home.php';
-        break;
-    case 'login':
-        require 'autentificacion/login.php';
-        break;
-    case 'registro':
-        require 'autentificacion/register.php';
-        break;
-    case 'email-verification':
-        require 'autentificacion/email-verification.php';
-        break;
-    case 'password-reset-2':
-        require 'autentificacion/password-reset-2.php';
-        break;
-    case 'password-reset':
-        require 'autentificacion/password-reset.php';
-        break;
+// Si la página solicitada está en $noLayoutPages, carga solo el contenido sin layout
+if (in_array($page, $noLayoutPages)) {
+    switch ($page) {
+      // -------------------------------------
+    //inicio autentificacion
+        case 'login':
+            require 'autentificacion/login.php';
+            break;
+        case 'registro':
+            require 'autentificacion/register.php';
+            break;
+        case 'email-verification':
+            require 'autentificacion/email-verification.php';
+            break;
+        case 'password-reset-2':
+            require 'autentificacion/password-reset-2.php';
+            break;
+        case 'password-reset':
+            require 'autentificacion/password-reset.php';
+            break;
+
+        case 'static-non-auth':
+            require 'usuario/static-non-auth.php';
+            break;
+        // Agrega más páginas que no extiendan el layout si es necesario
+        default:
+            require 'no-found.php'; // Página no encontrada
+            break;
+    }
+} else {
+    // Capturar el contenido de la página en un buffer si utiliza layout
+    ob_start(); 
+    switch ($page) {
+        // case 'inicio':
+        //     require 'layout.php';
+        //     break;
+        case 'home':
+            require 'home.php';
+            break;
 // -------------------------------------
     // inicio usuario
-    case 'settings':
-        require 'usuario/settings.php';
-        break;
-    case 'static-non-auth':
-        require 'usuario/static-non-auth.php';
-        break;
-    case 'user-edit':
-        require 'usuario/user-edit.php';
-        break;
-    case 'users':
-        require 'usuario/users.php';
-        break;
+        case 'settings':
+            require 'usuario/settings.php';
+            break;
+   
+        case 'user-edit':
+            require 'usuario/user-edit.php';
+            break;
+        case 'users':
+            require 'usuario/users.php';
+            break;
 // -------------------------------------
-    // Añadir más casos según sea necesario
-    default:
-        require 'no-found.php'; // Página no encontrada
-        break;
+        default:
+            require 'no-found.php'; // Página no encontrada
+            break;
+    }
+    $content = ob_get_clean();
+
+    // Incluir el archivo de layout si no está en las páginas excluidas
+    require 'layout.php';
 }
 ?>
-
-
-
